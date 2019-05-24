f665b9ddf324833bc38f877db3c5fe337b85ca56
Wilson Lewis <wilsonlewis@gmail.com>
2019-05-24 12:05:58 -0400

    Save Noise

--- scripts/shaders/noise.fs
+++ scripts/shaders/noise.fs
@@ -57,7 +57,6 @@ float posX = u_mouse.x * 0.1;
 float posY = u_mouse.y * 0.1;
 vec2 mouseUV = u_mouse / u_resolution;
 
-
 vec3 hsv2rgb(vec3 c){
   vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
   vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
