5420454b360a54ae1d69ee47880ccf1749c8015b
Wilson Lewis <wilsonlewis@gmail.com>
2019-05-29 08:40:42 -0400

    Reset Animation

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,183 @@
 import '../styles/all.scss'
-import fs from './shaders/ashima.fs'
-import vs from './shaders/ashima.vs'
+// vertex shader
+const vs = `
+attribute vec4 position;
+
+void main() {
+  gl_Position = position;
+}
+`;
+
+//fragment shader
+const fs = `
+// Author: Tobias Toft
+// Title: Mountain ridges for Quarter Studio, v. 0.0.1
+// Heavily inspired by and based on the 'noise holes' shader found here: https://www.shadertoy.com/view/XdyXz3
+
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+uniform sampler2D u_texture;
+uniform sampler2D u_ashima1;
+uniform sampler2D u_ashima2;
+
+// Generic random
+float random(vec2 st) {
+  float a = 12.9898;
+  float b = 78.233;
+  float c = 43758.5453;
+  float dt= dot(st.xy ,vec2(a,b));
+  float sn= mod(dt,3.14);
+  return fract(sin(sn) * c);
+}
+
+float whiteNoise(in vec2 p){
+  vec2 tv = p * vec2(u_resolution.x/256., u_resolution.y/256.); // * vec2(1.,float(u_resolution.y/u_resolution.x));
+  return (texture2D(u_texture, tv).r);
+}
+
+// Perlin noise
+float snoise(in vec2 p){
+  vec2 tv = p * vec2(u_resolution.x/2048., u_resolution.y/2048.); // * vec2(1.,float(u_resolution.y/u_resolution.x));
+  vec4 tex = texture2D(u_ashima1, tv);
+  //float val = (tex.r + tex.g + tex.b)/3.;
+  return tex.r;
+}
+
+// Perlin noise with two octaves
+float snoise2(in vec2 p){
+  vec2 tv = p * vec2(u_resolution.x/2048., u_resolution.y/2048.); // * vec2(1.,float(u_resolution.y/u_resolution.x));
+  vec4 tex = texture2D(u_ashima2, tv);
+  //float val = (tex.r + tex.g + tex.b)/3.;
+  return tex.r;
+}
+
+//--
+
+const float STEPS = 4.;
+const float LINE_WIDTH = 0.002;
+const float CUTOFF = 0.5;
+float posX = u_mouse.x * 0.1;
+float posY = u_mouse.y * 0.1;
+vec2 mouseUV = u_mouse / u_resolution;
+
+
+vec3 hsv2rgb(vec3 c){
+  vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+  vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+  return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+mat2 rotate2d(float a){
+  return mat2(
+      cos(a), -sin(a),
+      sin(a), cos(a)
+  );
+}
+
+float getNoise(vec2 uv, float t){
+  //given a uv coord and time - return a noise val in range 0 - 1
+  //using baked ashima noise
+
+  //octave 1
+  const float SCALEX = 1.;
+  const float SCALEY = 1.;
+  //float noise = snoise( vec2(uv.x * SCALEX + posX + t, (uv.y * SCALEY + t)));
+  float noise = snoise( rotate2d(sin(t*0.1) + posY * 2.) * vec2(uv.x * SCALEX + posX + t, uv.y * SCALEY + posY + t));
+
+  //octave 2
+  // NOTE: Disabling mixing two noise textures for now until I find a higher res/bitdepth way of doing it
+  //noise += snoise2( vec2(uv.x * SCALEX + t, uv.y * SCALEY + t)) * 0.2 ;
+
+  //move noise into 0 - 1 range
+  //noise = (noise/2. + 0.5);
+
+  return noise;
+}
+
+float getDepth(float n){
+  //remap remaining non-cutoff region to 0 - 1
+  //float cutoff = posY + 0.5;
+  float d = (n - CUTOFF) / (1. - CUTOFF);
+
+  //step
+  d = floor(d*STEPS)/STEPS;
+
+  return d;
+}
+
+void main(){
+  float t = u_time * 0.01;
+  vec2 uv = gl_FragCoord.xy / u_resolution;
+  vec3 col = vec3(0);
+
+  float noise = getNoise(uv, t);
+  float d = getDepth(noise);
+
+  //calc HSV color
+  float h = 0.5;// + (d * 0.3); //= d + 0.2; //rainbow hue
+  float s = 0.;
+  float v = 0.; //deeper is darker
+
+  // //get depth at offset position (needed for outlining)
+  // float noiseOffY = getNoise(uv + vec2(0, LINE_WIDTH), t);
+  // float noiseOffX = getNoise(uv + vec2(LINE_WIDTH, 0), t);
+  // float dOffY = getDepth(noiseOffY);
+  // float dOffX = getDepth(noiseOffX);
+
+  float WIDEN = 1. + (sin(t)+1.)/2. * 1.5;
+  const int STEPS = 16;
+  for (int j=0; j<STEPS; j++){
+    vec2 dOffset = vec2(
+      getDepth(getNoise(uv + vec2(0, LINE_WIDTH) * WIDEN * pow(float(j), 1.25), t)),
+      getDepth(getNoise(uv + vec2(0, -LINE_WIDTH) * WIDEN * pow(float(j), 1.25), t))
+    );
+
+
+    // // Save for later
+    // vec4 dOffset = vec4(
+    //   getDepth(getNoise(uv + vec2(LINE_WIDTH, 0) * WIDEN * pow(float(j), 1.25), t)),
+    //   getDepth(getNoise(uv + vec2(0, LINE_WIDTH) * WIDEN * pow(float(j), 1.25), t)),
+    //   getDepth(getNoise(uv + vec2(-LINE_WIDTH, 0) * WIDEN * pow(float(j), 1.25), t)),
+    //   getDepth(getNoise(uv + vec2(0, -LINE_WIDTH) * WIDEN * pow(float(j), 1.25), t))
+    // );
+
+
+    if (d != dOffset.x || d != dOffset.y){
+    //if (d != dOffset.x || d != dOffset.y || d != dOffset.z || d != dOffset.w){ // Save for later
+      h = 0.; //(uv.x * 0.1) + 0.5;
+      s = 0.;
+      v += .0625 * floor( whiteNoise(uv*2.) + 0.2 * float(STEPS)/float(j+1)); // + (0.2 * float(STEPS)/float(j+1)) );
+      if (d != dOffset.x){
+        v *= 0.75;
+      }
+    }
+  }
+
+  // // // Outline ridges
+  // if (d != dOffX || d != dOffY){
+  //   h = (d * 0.1) + (sin(t) + 1.)/2.;
+  //   s = .3;
+  //   v = (sin(t)+1.)/2. + d + 0.5;
+  //   //v *= 0.25;
+  // }
+
+  col = hsv2rgb(vec3(h,s,v));
+
+  //add vertical gradient
+  col *= 0.2 + (gl_FragCoord.y/u_resolution.y) * 0.8;
+
+  //add noise texture
+  //col += 0.1 * whiteNoise(uv + random(vec2(u_time, 0)));
+
+  gl_FragColor = vec4(col, 1.0);
+  //gl_FragColor = vec4(noise, noise, noise, 1.0);
+}
+`;
 
 // get mouse position for GL uniform
 function getRelativeMousePosition(event, target) {
@@ -24,16 +201,32 @@ function getNoPaddingNoBorderCanvasRelativeMousePosition(event, target) {
   return pos;
 }
 
+// resize canvas
+function resize(canvas) {
+  // Lookup the size the browser is displaying the canvas.
+  var displayWidth  = canvas.clientWidth;
+  var displayHeight = canvas.clientHeight;
+
+  // Check if the canvas is not the same size.
+  if (canvas.width  !== displayWidth ||
+      canvas.height !== displayHeight) {
+
+    // Make the canvas the same size
+    canvas.width  = displayWidth;
+    canvas.height = displayHeight;
+  }
+}
+
+
 // init webgl
-let top = 0
 const mousePos = {x: 0, y: 0};
+const gl = document.getElementById("c").getContext("webgl");
+const programInfo = twgl.createProgramInfo(gl, [vs, fs]);
+
 const arrays = {
   position: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
 };
 
-const canvas = document.querySelector(".shader");
-const gl = canvas.getContext("webgl");
-const programInfo = twgl.createProgramInfo(gl, [vs, fs]);
 const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
 const textures = twgl.createTextures(gl, {
   noise: { src: "/images/noise.png", mag: gl.NEAREST },
@@ -41,85 +234,39 @@ const textures = twgl.createTextures(gl, {
   ashima2: { src: "/images/ashima2.png" }
 });
 
-gl.useProgram(programInfo.program);
-twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
-
-window.uniforms = {
-  u_texture: textures.noise,
-  u_ashima1: textures.ashima1,
-  u_ashima2: textures.ashima2,
-  // u_option_knob: 0.5,
-  // u_option_size: 0.5,
-  // u_option_layers: 0.16,
-  // u_option_stroke: 0.2,
-  // u_option_spread: 0.1,
-  // u_option_fade: 0.625,
-  // u_option_edge: 0.6875,
-  // u_option_noise: 0.2,
-  // u_option_noise_size: 0.5,
-  // u_option_speed: 0.05,
-  // u_option_blend: 0.05,
-};
-
 function render(time) {
-  requestAnimationFrame(render);
+  twgl.resizeCanvasToDisplaySize(gl.canvas, 1); //use window.devicePixelRatio if you want retina, we might not though...
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-  if (top > canvas.height) return;
+  const uniforms = {
+    u_time: time * 0.001,
+    u_resolution: [gl.canvas.width, gl.canvas.height],
+    u_mouse: [mousePos.x, mousePos.y],
+    u_texture: textures.noise,
+    u_ashima1: textures.ashima1,
+    u_ashima2: textures.ashima2
+  };
 
-  uniforms.u_time = time;
-
+  gl.useProgram(programInfo.program);
+  twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
   twgl.setUniforms(programInfo, uniforms);
   twgl.drawBufferInfo(gl, bufferInfo);
+
+  requestAnimationFrame(render);
 }
 
 requestAnimationFrame(render);
 
-// attach listener for resizing the canvas
-function resize() {
-  canvas.width  = canvas.clientWidth;
-  canvas.height = canvas.clientHeight;
-  uniforms.u_resolution = [canvas.width, canvas.height];
-  twgl.resizeCanvasToDisplaySize(canvas, 1); //use window.devicePixelRatio if you want retina, we might not though...
-  gl.viewport(0, 0, canvas.width, canvas.height);
-}
-
-resize();
-
-window.addEventListener('resize', resize);
 
 // attach listener for updating mouse position uniform
-function mousemove (e) {
-  const pos = getNoPaddingNoBorderCanvasRelativeMousePosition(e, canvas);
+window.addEventListener('mousemove', e => {
+  const pos = getNoPaddingNoBorderCanvasRelativeMousePosition(e, gl.canvas);
+
   // pos is in pixel coordinates for the canvas.
   // so convert to WebGL clip space coordinates
-  const x = pos.x / canvas.width  *  2 - 1;
-  const y = pos.y / canvas.height * -2 + 1;
+  const x = pos.x / gl.canvas.width  *  2 - 1;
+  const y = pos.y / gl.canvas.height * -2 + 1;
 
-  mousePos.x = -0.02; // x;
-  mousePos.y = 0.015; // y;
-
-  uniforms.u_mouse = [mousePos.x, mousePos.y];
-}
-
-window.addEventListener('mousemove', mousemove);
-
-// attach listener for disable render on scroll
-function scroll (e) {
-  var doc = document.documentElement; //IE with doctype
-  doc = doc.clientHeight ? doc : document.body;
-  top = doc.scrollTop
-}
-
-window.addEventListener('scroll', scroll)
-
-// attach listener for loading the editor with the tilde key
-// function keydown (e) {
-//   if (e.keyCode === 192) {
-//     var script = document.createElement('script')
-//     script.src = '/scripts/editor.js'
-//     document.body.appendChild(script)
-//     window.removeEventListener('keydown', keydown)
-//   }
-// }
-
-// window.addEventListener('keydown', keydown)
+  // mousePos.x = x;
+  // mousePos.y = y;
+});
